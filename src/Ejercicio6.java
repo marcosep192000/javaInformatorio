a5959423eb1b421ee37653651a1b1bd245dd339f
Marcos <69874820+marcosep192000@users.noreply.github.com>
2021-10-24 13:59:03 -0300

    24-10

---
+++ src/Ejercicio6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        String palabra;
+        char caracter; ;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Ingresá una palabra para convertir a MAYUSCULAS: ");
+        System.out.println("-----------------********----------------------- ");
+        palabra = reader.nextLine();
+        StringBuilder st = new StringBuilder();
+        for (int i = 0; i < palabra.length(); i++) {
+            caracter = palabra.charAt(i);
+            if (caracter >= 'a' && caracter <= 'z') {
+                caracter= (char)(caracter - (int) 'a' + (int) 'A');
+                st.append(caracter);
+            }
+        }
+        System.out.println(st);
+        }
+}
---
+++ src/Ejercicio7.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args)
+    {
+        String nomApe,direccion,ciudad;
+        Integer numero;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("               Ingresá Nombre y Apellido                 ");
+        System.out.println("------------------------********------------------------ ");
+        nomApe = scanner.nextLine();
+        System.out.println("                    Ingresá direccion                    ");
+        System.out.println("------------------------********------------------------ ");
+        direccion= scanner.nextLine();
+        System.out.println("                    Ingresá tu Ciudad                    ");
+        System.out.println("------------------------********------------------------ ");
+        ciudad= scanner.nextLine();
+            System.out.println("         Ingresá tu  NUMERO de vivivienda            ");
+        System.out.println("------------------------********------------------------ ");
+        numero= scanner.nextInt();
+        scanner.close();
+
+        List list = new ArrayList();
+
+        list.add(nomApe);
+        list.add(direccion);
+        list.add(ciudad);
+        list.add(numero);
+
+        System.out.println(" :::::::::::::::::INFORMACION:::::::::::::::    ");
+
+        System.out.println( list.toString().replace("[", "").replace("]", "").replace(",", " -"));
+
+
+
+
+
+    }
+}
